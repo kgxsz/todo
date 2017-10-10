a0011947db8e0c13194c199a9e6bffef529444eb
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-10 16:00:16 +0200

    Separate reducers

--- redux/src/reducers.js
+++ redux/src/reducers.js
@@ -1,68 +1,108 @@
 import { validateInputValue } from "./utils";
 
 const initialState = {
-  sortByDescAddedAt: false,
+  sortByDescAddedAt: true,
   itemsByAddedAt: {},
   itemList: [],
   inputValue: ""
 };
 
+const sortItemList = (itemList, sortByDescAddedAt) => {
+  itemList.sort(function(a, b) {
+    if (sortByDescAddedAt) {
+      return a > b ? -1 : a < b ? 1 : 0;
+    } else {
+      return a > b ? 1 : a < b ? -1 : 0;
+    }
+  });
+};
+
+const toggleSortByDescAddedAt = (state, action) => {
+  let sortByDescAddedAt = state.sortByDescAddedAt ? false : true;
+  let itemList = Object.assign([], state.itemList);
+  sortItemList(itemList, sortByDescAddedAt);
+  return {
+    ...state,
+    sortByDescAddedAt: sortByDescAddedAt,
+    itemList: itemList
+  };
+};
+
+const toggleItemChecked = (state, action) => {
+  return {
+    ...state,
+    itemsByAddedAt: {
+      ...state.itemsByAddedAt,
+      [action.addedAt]: {
+        ...state.itemsByAddedAt[action.addedAt],
+        checked: !state.itemsByAddedAt[action.addedAt].checked
+      }
+    }
+  };
+};
+
+const deleteItem = (state, action) => {
+  let itemsByAddedAt = Object.assign({}, state.itemsByAddedAt);
+  delete itemsByAddedAt[action.addedAt];
+
+  let itemList = Object.assign([], state.itemList);
+  let index = itemList.indexOf(action.addedAt);
+  if (index > -1) {
+    itemList.splice(index, 1);
+  }
+
+  return {
+    ...state,
+    itemList: itemList,
+    itemsByAddedAt: itemsByAddedAt
+  };
+};
+
+const updateInputValue = (state, action) => {
+  return {
+    ...state,
+    inputValue: action.inputValue
+  };
+};
+
+const addItemToItemList = (state, action) => {
+  if (validateInputValue(state.inputValue)) {
+    var item = {
+      addedAt: action.addedAt,
+      value: state.inputValue,
+      checked: false
+    };
+
+    let itemsByAddedAt = Object.assign({}, state.itemsByAddedAt);
+    itemsByAddedAt[item.addedAt] = item;
+
+    let itemList = Object.assign([], state.itemList);
+    itemList.push(item.addedAt);
+    sortItemList(itemList, state.sortByDescAddedAt);
+
+    return {
+      ...state,
+      inputValue: "",
+      itemsByAddedAt: itemsByAddedAt,
+      itemList: itemList
+    };
+  } else {
+    return state;
+  }
+};
+
 const appReducer = (state = initialState, action) => {
   switch (action.type) {
     case "TOGGLE_SORT_BY_DESC_ADDED_AT":
-      return state.sortByDescAddedAt ? false : true;
-
+      return toggleSortByDescAddedAt(state, action);
     case "TOGGLE_ITEM_CHECKED":
-      return {
-        ...state,
-        itemsByAddedAt: {
-          ...state.itemsByAddedAt,
-          [action.addedAt]: {
-            ...state.itemsByAddedAt[action.addedAt],
-            checked: !state.itemsByAddedAt[action.addedAt].checked
-          }
-        }
-      };
-
+      return toggleItemChecked(state, action);
     case "DELETE_ITEM":
-      let itemsByAddedAt = Object.assign({}, state.itemsByAddedAt);
-      let index = state.itemList.indexOf(action.addedAt);
-      delete itemsByAddedAt[action.addedAt];
-      return {
-        ...state,
-        itemList: [
-          ...state.itemList.slice(0, index),
-          ...state.itemList.slice(index + 1)
-        ],
-        itemsByAddedAt: itemsByAddedAt
-      };
-
+      return deleteItem(state, action);
     case "UPDATE_INPUT_VALUE":
-      return {
-        ...state,
-        inputValue: action.inputValue
-      };
-
+      return updateInputValue(state, action);
     case "ADD_ITEM_TO_ITEM_LIST":
-      if (validateInputValue(state.inputValue)) {
-        let item = {
-          addedAt: action.addedAt,
-          value: state.inputValue,
-          checked: false
-        };
-        return {
-          ...state,
-          inputValue: "",
-          itemsByAddedAt: {
-            ...state.itemsByAddedAt,
-            [item.addedAt]: item
-          },
-          itemList: [item.addedAt, ...state.itemList.slice(0)]
-        };
-      } else {
-        return state;
-      }
-
+      return addItemToItemList(state, action);
     default:
       return state;
   }
