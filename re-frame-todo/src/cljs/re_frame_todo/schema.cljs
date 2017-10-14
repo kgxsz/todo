05002227aa0002995daf12e66ce2b65cdc163e7c
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-14 18:26:35 +0200

    Add a schema for the db

---
+++ re-frame-todo/src/cljs/re_frame_todo/schema.cljs
@@ -0,0 +1,21 @@
+(ns re-frame-todo.schema
+  (:require [cljs.spec.alpha :as spec]
+            [clojure.string :as str]))
+
+(spec/def ::added-at int?)
+
+(spec/def ::text (spec/and string? (complement str/blank?)))
+
+(spec/def ::checked? boolean?)
+
+(spec/def ::item (spec/keys :req-un [::added-at ::text ::checked?]))
+
+(spec/def ::items-by-added-at (spec/and map? (spec/map-of ::added-at ::item)))
+
+(spec/def ::item-list (spec/coll-of ::added-at))
+
+(spec/def ::input-value string?)
+
+(spec/def ::sort-by-desc-added-at boolean?)
+
+(spec/def ::db (spec/keys :req-un [::items-by-added-at ::item-list ::input-value ::sort-by-desc-added-at]))
