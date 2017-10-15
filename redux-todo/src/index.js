96875edca17afdd47e27c4a4a221adfa2b556d21
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-15 09:25:59 +0200

    Add normalize.css to redux-todo

--- redux-todo/src/index.js
+++ redux-todo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import appReducer from "./reducers";
+import "normalize.css";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
