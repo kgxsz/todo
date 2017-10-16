214030e56aab78929bc4c72fda443428a96f22db
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-16 17:48:41 +0200

    Weave initial state through to item-adder

--- fulcro-todo/src/main/app/ui.cljs
+++ fulcro-todo/src/main/app/ui.cljs
@@ -1,28 +1,42 @@
 (ns app.ui
   (:require [om.dom :as dom]
-            [om.next :as om :refer [defui]]))
+            [om.next :as om :refer [defui]]
+            [fulcro.client.core :as fc]))
 
 (defui ^:once ItemAdder
+  static
+  fc/InitialAppState
+  (initial-state [c params] {:input-value ""
+                             :valid-input-value? false})
   Object
   (render [this]
-          (dom/form
-           #js {:className "item-adder"
-                :onSubmit #(.preventDefault %)}
-           (dom/input
-            #js {:className "item-adder__input"
-                 :type "text"
-                 :value "hey"})
-           (dom/input
-            #js {:className "item-adder__button"
-                 :type "submit"
-                 :value "add"}))))
+          (let [{:keys [input-value valid-input-value?]} (om/props this)]
+            (dom/form
+             #js {:className "item-adder"
+                  :onSubmit #(.preventDefault %)}
+             (dom/input
+              #js {:className "item-adder__input"
+                   :type "text"
+                   :value input-value})
+             (dom/input
+              #js {:className (if valid-input-value?
+                                "item-adder__button"
+                                "item-adder__button item-adder__button--disabled")
+                   :type "submit"
+                   :value "add"
+                   :disabled (not valid-input-value?)})))))
 
 (def ui-item-adder (om/factory ItemAdder))
 
 (defui ^:once App
+  static
+  fc/InitialAppState
+  (initial-state [c params] {:item-adder (fc/get-initial-state ItemAdder {})})
+
   Object
   (render [this]
-          (let [{:keys [ui/react-key]} (om/props this)]
+          (let [{:keys [ui/react-key]} (om/props this)
+                {:keys [item-adder]} (fc/get-initial-state App {})]
             (dom/div
              #js {:key react-key
                   :className "app"}
@@ -38,6 +52,6 @@
               #js {:className "app__body"}
               (dom/div
                #js {:className "app__body__divider"})
-              (ui-item-adder)
+              (ui-item-adder item-adder)
               (dom/div
                #js {:className "app__body__divider"}))))))
