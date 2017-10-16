c938506ed32241b814a25cdd5f09a0d432542738
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-16 13:22:47 +0200

    Minor styling tweaks

--- fulcro-todo/src/dev/cljs/user.cljs
+++ fulcro-todo/src/dev/cljs/user.cljs
@@ -1,7 +1,6 @@
 (ns cljs.user
-  (:require
-   [app.ui :refer [app ui-app]]
-   [fulcro.client.core :as fc]))
+  (:require [app.ui :refer [app ui-app]]
+            [fulcro.client.core :as fc]))
 
 (defn refresh [] (swap! app fc/mount ui-app "app"))
 
