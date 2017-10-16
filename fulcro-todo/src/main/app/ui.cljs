45cda9a3598f1f5d8a92a1c9916057b707a0bfb9
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-16 13:51:52 +0200

    Styled skeleton in place

--- fulcro-todo/src/main/app/ui.cljs
+++ fulcro-todo/src/main/app/ui.cljs
@@ -8,4 +8,19 @@
 (defui App
   Object
   (render [this]
-          (dom/div nil "Hello World.")))
+          (dom/div
+           #js {:className "app"}
+           (dom/div
+            #js {:className "app__header"}
+            (dom/div
+             #js {:className "app__header__title"}
+             "Todo List")
+            (dom/div
+             #js {:className "app__header__subtitle"}
+             "Developed with Fulcro"))
+           (dom/div
+            #js {:className "app__body"}
+            (dom/div
+             #js {:className "app__body__divider"})
+            (dom/div
+             #js {:className "app__body__divider"})))))
