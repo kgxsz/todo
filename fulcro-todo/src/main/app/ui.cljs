06411912663256f66525f36ecebea210f0b2747e
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-16 16:44:12 +0200

    Get hot code reloading working with a top level key and ^:once

--- fulcro-todo/src/main/app/ui.cljs
+++ fulcro-todo/src/main/app/ui.cljs
@@ -2,22 +2,42 @@
   (:require [om.dom :as dom]
             [om.next :as om :refer [defui]]))
 
-(defui App
+(defui ^:once ItemAdder
   Object
   (render [this]
-          (dom/div
-           #js {:className "app"}
-           (dom/div
-            #js {:className "app__header"}
+          (dom/form
+           #js {:className "item-adder"
+                :onSubmit #(.preventDefault %)}
+           (dom/input
+            #js {:className "item-adder__input"
+                 :type "text"
+                 :value "hey"})
+           (dom/input
+            #js {:className "item-adder__button"
+                 :type "submit"
+                 :value "add"}))))
+
+(def ui-item-adder (om/factory ItemAdder))
+
+(defui ^:once App
+  Object
+  (render [this]
+          (let [{:keys [ui/react-key]} (om/props this)]
             (dom/div
-             #js {:className "app__header__title"}
-             "Todo List")
-            (dom/div
-             #js {:className "app__header__subtitle"}
-             "Developed with Fulcro"))
-           (dom/div
-            #js {:className "app__body"}
-            (dom/div
-             #js {:className "app__body__divider"})
-            (dom/div
-             #js {:className "app__body__divider"})))))
+             #js {:key react-key
+                  :className "app"}
+             (dom/div
+              #js {:className "app__header"}
+              (dom/div
+               #js {:className "app__header__title"}
+               "Todo List")
+              (dom/div
+               #js {:className "app__header__subtitle"}
+               "Developed with Fulcro"))
+             (dom/div
+              #js {:className "app__body"}
+              (dom/div
+               #js {:className "app__body__divider"})
+              (ui-item-adder)
+              (dom/div
+               #js {:className "app__body__divider"}))))))
