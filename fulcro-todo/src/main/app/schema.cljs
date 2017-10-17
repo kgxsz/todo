a6167a4bf82cf103463ccb38f55eb6095ec87d33
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-17 10:05:16 +0200

    Add schema

---
+++ fulcro-todo/src/main/app/schema.cljs
@@ -0,0 +1,23 @@
+(ns app.schema
+  (:require [cljs.spec.alpha :as spec]
+            [clojure.string :as str]))
+
+(spec/def ::added-at int?)
+
+(spec/def ::text (spec/and string?
+                           (complement str/blank?)))
+
+(spec/def ::checked? boolean?)
+
+(spec/def ::item (spec/keys :req-un [::added-at
+                                     ::text
+                                     ::checked?]))
+
+(spec/def ::items-by-added-at (spec/and map?
+                                        (spec/map-of ::added-at ::item)))
+
+(spec/def ::item-list (spec/coll-of ::added-at))
+
+(spec/def ::input-value string?)
+
+(spec/def ::sort-by-desc-added-at? boolean?)
